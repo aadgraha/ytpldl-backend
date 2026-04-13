f51774911076173e7aff9de9f8a836e7545055f8
Yahya Adi <adiyahya454@gmail.com>
2026-04-13 15:07:00 +0700

    refactor logic

--- main.py
+++ main.py
@@ -11,14 +11,12 @@ os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 
 @app.get("/download")
-def download(url: str, mode: str = "single_audio"):
+def download(url: str, mode: str = "video"):
     folder_id = str(uuid.uuid4())
     folder_path = os.path.join(DOWNLOAD_DIR, folder_id)
     os.makedirs(folder_path, exist_ok=True)
 
-    # ---------------- MODE HANDLING ---------------- #
-
-    if mode == "single_audio":
+    if mode == "audio":
         command = [
             "yt-dlp",
             "--extract-audio",
@@ -26,39 +24,12 @@ def download(url: str, mode: str = "single_audio"):
             "--audio-quality", "0",
             "--embed-thumbnail", 
             "--add-metadata",
-            "--no-playlist",
             "-o", os.path.join(folder_path, "%(title)s.%(ext)s"),
             "--restrict-filenames",
             url
         ]
 
-    elif mode == "single_video":
-        command = [
-            "yt-dlp",
-            "-f", "bv*[ext=mp4]+ba[ext=m4a]/mp4",
-            "--merge-output-format", "mp4",
-            "--no-playlist",
-            "-o", os.path.join(folder_path, "%(title)s.%(ext)s"),
-            "--restrict-filenames",
-            url
-        ]
-
-    elif mode == "playlist_audio":
-        command = command = [
-            "yt-dlp",
-            "--extract-audio",
-            "--audio-format", "m4a",
-            "--audio-quality", "0",
-            "--embed-thumbnail", 
-            "--add-metadata",
-            "--yes-playlist",
-            "--ignore-errors",
-            "-o", os.path.join(folder_path, "%(title)s.%(ext)s"),
-            "--restrict-filenames",
-            url
-        ]
-
-    elif mode == "playlist_video":
+    elif mode == "video":
         command = [
             "yt-dlp",
             "-f", "bv*[ext=mp4]+ba[ext=m4a]/mp4",
@@ -71,11 +42,7 @@ def download(url: str, mode: str = "single_audio"):
     else:
         return {"error": "invalid mode"}
 
-    # ---------------- RUN ---------------- #
-
     subprocess.run(command)
-
-    # ---------------- COLLECT FILES ---------------- #
 
     files = sorted([
         f for f in os.listdir(folder_path)
@@ -86,7 +53,6 @@ def download(url: str, mode: str = "single_audio"):
         "folder": folder_id,
         "files": files,
     }
-
 
 @app.get("/file/{folder}/{filename}")
 def get_file(folder: str, filename: str):
